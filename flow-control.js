d1bb57911436cdbdef506e142fe9ecf4ac2e6c55
Kyle Crews <kyledcrews@gmail.com>
2019-03-08 20:33:15 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,3 +23,12 @@ function ageChecker(age) {
     }
 
 }
+
+function ternaryTeenager(age) {
+  return (age >=13 && age <=19) ?|
+    "You are a teenager!" : "You are not a teenager"
+}
+
+function switchAge(age) {
+
+}
