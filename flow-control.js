57358d2ca5d32784dad07704f866fa41346908d7
Kyle Crews <kyledcrews@gmail.com>
2019-03-08 21:02:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,24 +32,5 @@ function switchAge(age) {
   switch(age) {
     case (age >=13 && age <=19):
       return "You are a teenager";
-      break;
-    case "14":
-      return "You are a teenager";
-      break;
-    case "15":
-      return "You are a teenager";
-      break;
-    case "16":
-      return "You are a teenager";
-      break;
-    case "17":
-      return "You are a teenager";
-      break;
-    case "18":
-      return "You are a teenager";
-      break;
-    case "19":
-      return "You are a teenager";
-      break;
   }
 }
